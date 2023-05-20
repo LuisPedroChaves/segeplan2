21bf1ae9624cbb3e51119e14fa1df0ed7402b694
LuisPedroChaves <luispedrochaves@hotmail.com>
2023-05-19 19:01:24 -0600

    Corrección de filtro

--- src/app/modules/check-project/components/project-table/project-table.component.ts
+++ src/app/modules/check-project/components/project-table/project-table.component.ts
@@ -6,84 +6,86 @@ import { MatPaginator } from '@angular/material/paginator';
 
 import { IProject } from 'src/app/core/models/seguimiento/project';
 import { OPEN_DRAWER1 } from 'src/app/core/store/actions';
-import { READ_CHECK_PROJECTS, SET_PROJECT } from '../../store/actions/checkProject.actions';
+import {
+  READ_CHECK_PROJECTS,
+  SET_PROJECT,
+} from '../../store/actions/checkProject.actions';
 import { CheckProjectStore } from '../../store/reducers/checkProject.reducer';
 import { IFiltroCheckProjects } from 'src/app/core/models/adicionales';
 
 @Component({
   selector: 'app-project-table',
   templateUrl: './project-table.component.html',
-  styleUrls: ['./project-table.component.scss']
+  styleUrls: ['./project-table.component.scss'],
 })
 export class ProjectTableComponent implements OnInit, OnDestroy {
+  @Input('component') component: string = 'NEW_PROJECT';
 
-  @Input('component') component: string = 'NEW_PROJECT'
   @ViewChild(MatPaginator) paginator: MatPaginator;
   dataSource = new MatTableDataSource<IProject>([]);
-  displayedColumns = ['correlative', 'nameProject', 'ministry',  'departament', 'munic',  'advance', 'actions'];
+  displayedColumns = [
+    'correlative',
+    'nameProject',
+    'ministry',
+    'departament',
+    'munic',
+    'advance',
+    'actions',
+  ];
   checkProjectSubscription = new Subscription();
-  isMinistry = false;
 
-  filtros1: IFiltroCheckProjects = {
+  filtros: IFiltroCheckProjects = {
     isMinistry: false,
     status: 'REGISTER',
-  }
+  };
 
-  filtros2: IFiltroCheckProjects = {
-    isMinistry: false,
-    status: 'FINISHED',
-  }
-
-  constructor(
-    public checkProjectStore: Store<CheckProjectStore>,
-    ) {}
+  constructor(public checkProjectStore: Store<CheckProjectStore>) {}
 
   ngOnInit(): void {
-    this.checkProjectSubscription = this.checkProjectStore.select('checkProject')
-      .subscribe(state => {
-        this.isMinistry = state.isMinistry;
+    this.checkProjectSubscription = this.checkProjectStore
+      .select('checkProject')
+      .subscribe((state) => {
 
-        if (this.filtros1.isMinistry != state.isMinistry) {
-          this.checkProjectStore.dispatch(READ_CHECK_PROJECTS({
-            filtros: {
-              ...this.filtros1,
-              isMinistry: state.isMinistry
-            }
-          }))
+        if (this.filtros.isMinistry != state.isMinistry) {
+          this.filtros = {
+            ...this.filtros,
+            isMinistry: state.isMinistry,
+          };
+          this.checkProjectStore.dispatch(
+            READ_CHECK_PROJECTS({
+              filtros: this.filtros,
+            })
+          );
+          console.log(this.filtros);
         }
-        this.filtros1 = {
-          ...this.filtros1,
-          isMinistry: state.isMinistry
-        }
+
         this.dataSource = new MatTableDataSource<IProject>([...state.projects]);
+        setTimeout(() => {
+          this.dataSource.paginator = this.paginator;
+        });
       });
 
-    this.checkProjectSubscription = this.checkProjectStore.select('checkProject')
-    .subscribe(state => {
-        if (this.filtros2.isMinistry != state.isMinistry) {
-          this.checkProjectStore.dispatch(READ_CHECK_PROJECTS({
-            filtros: {
-              ...this.filtros2,
-              isMinistry: state.isMinistry
-            }
-          }))
-        }
-        this.filtros2 = {
-          ...this.filtros2,
-          isMinistry: state.isMinistry
-        }
-        this.dataSource = new MatTableDataSource<IProject>([...state.projects]);
-      });
+    if (this.component === 'PROJECT_LIST') {
+      this.filtros = {
+        ...this.filtros,
+        status: 'REGISTER',
+      };
+    }
 
-        if (this.component === 'PROJECT_LIST') {
-          this.checkProjectStore.dispatch(READ_CHECK_PROJECTS({ filtros: this.filtros1 }))
-        }
+    if (this.component === 'PROJECT_FINISHED') {
+      this.filtros = {
+        ...this.filtros,
+        status: 'FINISHED',
+      };
+    }
 
-        if (this.component === 'PROJECT_FINISHED') {
-          this.checkProjectStore.dispatch(READ_CHECK_PROJECTS({ filtros: this.filtros2 }))
-        }
+    console.log(this.filtros);
 
-        setTimeout(() => this.dataSource.paginator = this.paginator)
+    setTimeout(() => {
+      this.checkProjectStore.dispatch(
+        READ_CHECK_PROJECTS({ filtros: this.filtros })
+      );
+    }, 100);
   }
 
   ngOnDestroy(): void {
@@ -91,8 +93,7 @@ export class ProjectTableComponent implements OnInit, OnDestroy {
   }
 
   openDrawer1(width1: string, component1: string, checkProject: IProject) {
-    this.checkProjectStore.dispatch(SET_PROJECT({ checkProject }))
-    this.checkProjectStore.dispatch(OPEN_DRAWER1({ width1, component1 }))
+    this.checkProjectStore.dispatch(SET_PROJECT({ checkProject }));
+    this.checkProjectStore.dispatch(OPEN_DRAWER1({ width1, component1 }));
   }
-
 }
